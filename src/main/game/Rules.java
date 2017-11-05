b7e04b6c76969abd753610b60d402abc72b51efa
Katerina <katerinaldg@gmail.com>
2017-11-05 14:34:04 +0000

    Extracted logic to method that finds rock's score

--- src/main/game/Rules.java
+++ src/main/game/Rules.java
@@ -2,10 +2,11 @@ package main.game;
 
 public class Rules {
     public String scoreGame(String playerOneMove, String playerTwoMove) {
+
         if (playerOneMove.equals(playerTwoMove)) {
             return "draw";
-        } else if (playerOneMove.equals("rock") && playerTwoMove.equals("paper")) {
-            return "paper";
+        } else if (playerOneMove.equals("rock")) {
+            return findScoreForRock(playerTwoMove);
         } else if (playerOneMove.equals("paper") && playerTwoMove.equals("scissors")) {
             return "scissors";
         } else if (playerOneMove.equals("scissors") && playerTwoMove.equals("rock")) {
@@ -14,4 +15,13 @@ public class Rules {
             return playerOneMove;
         }
     }
+
+    public String findScoreForRock(String playerTwoMove) {
+        if (playerTwoMove.equals("paper")) {
+            return "paper";
+        } else {
+            return "rock";
+        }
+    }
+
 }
