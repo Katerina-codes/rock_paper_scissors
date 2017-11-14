ec834c6b9e16762c7bcb842988934534b6de2a56
Katerina <katerinaldg@gmail.com>
2017-11-14 14:59:45 +0000

    Added both game modes to Game flow

--- src/main/game/Game.java
+++ src/main/game/Game.java
@@ -12,13 +12,24 @@ public class Game {
     public void runGame() {
         inputOutput.askUserForGameMode();
         String gameMode = inputOutput.getGameMode();
-        inputOutput.askForMove("Player one");
-        String playerOneMove = inputOutput.getMoveFromUser();
-        inputOutput.askForMove("Player two");
-        String playerTwoMove = inputOutput.getMoveFromUser();
-        Moves convertedPlayerOneMove = CommandLineUI.stringToEnum(playerOneMove);
-        Moves convertedPlayerTwoMove = CommandLineUI.stringToEnum(playerTwoMove);
-        String winningMove = rules.scoreGame(convertedPlayerOneMove, convertedPlayerTwoMove);
-        inputOutput.announceWinner(winningMove);
+
+        if (gameMode.equals("1")) {
+            inputOutput.askForMove("Player one");
+            String playerOneMove = inputOutput.getMoveFromUser();
+            inputOutput.askForMove("Player two");
+            String playerTwoMove = inputOutput.getMoveFromUser();
+            Moves convertedPlayerOneMove = CommandLineUI.stringToEnum(playerOneMove);
+            Moves convertedPlayerTwoMove = CommandLineUI.stringToEnum(playerTwoMove);
+            String winningMove = rules.scoreGame(convertedPlayerOneMove, convertedPlayerTwoMove);
+            inputOutput.announceWinner(winningMove);
+        } else {
+            inputOutput.askForMove("Player one");
+            String playerOneMove = inputOutput.getMoveFromUser();
+            String playerTwoMove = ComputerPlayer.playRandomMove();
+            Moves convertedPlayerOneMove = CommandLineUI.stringToEnum(playerOneMove);
+            Moves convertedPlayerTwoMove = CommandLineUI.stringToEnum(playerTwoMove);
+            String winningMove = rules.scoreGame(convertedPlayerOneMove, convertedPlayerTwoMove);
+            inputOutput.announceWinner(winningMove);
+        }
     }
 }
