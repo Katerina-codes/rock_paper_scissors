dad176b4b0c6719f5aa91e8d820436cfa2b5fe7c
Katerina <katerinaldg@gmail.com>
2017-11-28 15:04:29 +0000

    Remove unnecessary instance variables

--- src/main/game/LanguageFactory.java
+++ src/main/game/LanguageFactory.java
@@ -1,19 +1,12 @@
 package main.game;
 
 public class LanguageFactory {
-    private final Greek greek;
-    public English english;
-
-    public LanguageFactory() {
-        english = new English();
-        greek = new Greek();
-    }
 
     public Language getLanguage(String languageDesired) {
         if (languageDesired.equals("English")) {
-            return english;
+            return new English();
         } else {
-            return greek;
+            return new Greek();
         }
     }
 }
